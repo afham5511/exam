611155fb710b96b8c0f237be4bcac3c1ea0c1b1a
afham5511 <afhamali5511@gmail.com>
2025-02-17 14:45:38 +0530

    Update connection.js

--- monsoonExit-main/monsoonExit-main/Backend/connection.js
+++ monsoonExit-main/monsoonExit-main/Backend/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose
   .connect(
-   'mongodb+srv://advaith@cluster0.qerfx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+   'mongodb+srv://afhamali5477:afham@cluster0.avyta.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
   , {dbName:"examBlog"})
   .then(() => {
     console.log("Connected to DB");
