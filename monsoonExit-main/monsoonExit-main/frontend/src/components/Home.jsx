cb82d20e462f948ebb2df0e5a31d7d0f6514aa44
afham5511 <afhamali5511@gmail.com>
2025-02-17 12:40:48 +0530

    Add files via upload

---
+++ monsoonExit-main/monsoonExit-main/frontend/src/components/Home.jsx
@@ -0,0 +1,15 @@
+//Create the Home UI for the BlogAPP(Cards are preferrred; You may choose your UI preference )
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+ 
+    </div>
+  )
+}
+
+export default Home
+
+
+//Write your code here
